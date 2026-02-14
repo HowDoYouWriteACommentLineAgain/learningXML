b03878a4688dcda75799622abfa25b90435845bc
CIITSHSDTP00\CIIT <it@ciit.edu.ph>
2026-02-14 18:01:44 +0800

    init

---
+++ app/src/main/java/com/example/learning/SplashScreen.kt
@@ -0,0 +1,24 @@
+package com.example.learning
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.learning.databinding.ActivitySplashScreenBinding
+
+class SplashScreen : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashScreenBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.startBtn.setOnClickListener{
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+    }
+}
